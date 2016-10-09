aeee7b38e05bfd9b05b01d6c51a61e8db1489f19
mokx <ianbuena081191@gmail.com>
2016-10-09 18:59:18 +0800

    Added vertical scrolling Made wasd keys work like directional keys Made the scrolling smooth

--- content.js
+++ content.js
@@ -1,15 +1,48 @@
+// scroll reference
+let scrollMagic, scrollFlag = false
+
 window.addEventListener('keydown', event => {
   try {
-    switch (event.keyCode) {
-      case 37:
-        document.querySelector("#containerRoot > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1)").click()
-        break;
-      case 39:
-        document.querySelector("#containerRoot > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > a:nth-child(2)").click()
-        break;
+    if (!scrollFlag) {
+      scrollFlag = true
+      switch (event.keyCode) {
+        case 37:
+        case 65:
+          event.preventDefault()
+          scrollMagic = setInterval(() => {
+            document.querySelector("#containerRoot > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1)").click()
+          }, 20)
+          break;
+        case 39:
+        case 68:
+          event.preventDefault()
+          scrollMagic = setInterval(() => {
+            document.querySelector("#containerRoot > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > a:nth-child(2)").click()
+          }, 20)
+          break;
+        case 40:
+        case 83:
+          event.preventDefault()
+          scrollMagic = setInterval(() => {
+            window.scrollBy(0,13)
+          }, 20)
+          break
+        case 38:
+        case 87:
+          event.preventDefault()
+          scrollMagic = setInterval(() => {
+            window.scrollBy(0,-13)
+          }, 20)
+          break
+      }
     }
   } catch (err) {
     console.warn("Can't find node for prev/next page")
   }
 })
 
+
+window.addEventListener('keyup', event => {
+  scrollMagic = window.clearInterval(scrollMagic)
+  scrollFlag = false
+})
